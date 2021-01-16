220bf4bac7fc360e65bfc215afe2c7007fe3ecb9
sudhanshu2807 <sudhanshuraj8651475233@gmail.com>
2021-01-16 18:00:32 +0530

    saturday work

---
+++ addition3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{int a,b,result;
+
+printf("enter the first value = \n");
+scanf("%d",&a);
+printf("enter the second value = \n");
+scanf("%d",&b);
+result = a+b;
+printf("value after a + b is %d \n",result);
+return 0;
+}
