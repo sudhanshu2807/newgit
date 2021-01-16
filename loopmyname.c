0787b90bc1dc2e15939f249ee5e5fd7c49df71ec
sudhanshu2807 <sudhanshuraj8651475233@gmail.com>
2021-01-16 23:37:49 +0530

    today work

---
+++ loopmyname.c
@@ -0,0 +1,18 @@
+/* while loop in C 
+while (condition) */
+#include<stdio.h>
+int main()
+{
+int n,a;
+a = 0;
+printf("enter the value = ");
+scanf("%d",&n);
+
+while (a < n)
+   {
+   	printf("my name is sudhanshu raj\n");
+   	a++;
+   	
+   }
+return 0;	
+}
